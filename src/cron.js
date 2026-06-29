6f437440cce9f811c8233bba01344809daaa9ac8
Paritoshpardeshi35@gmail.com <87633752+pari35@users.noreply.github.com>
2026-06-29 13:39:26 +0530

    tc

--- src/cron.js
+++ src/cron.js
@@ -33,7 +33,7 @@ function createTransporter() {
 const scheduleCronJobs = () => {
     // Logs when the cron job is scheduled and active
     // cron.schedule('*/1 * * * *', async () => {
-    cron.schedule('35 13 * * *', async () => {
+    cron.schedule('42 13 * * *', async () => {
         logger.info('Cron job starting: Sending Birthday Mail...');
         try {
 
